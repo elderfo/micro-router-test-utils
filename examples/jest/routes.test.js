78a09d9b34e6411061596ec15cc45f22e0bddd3b
Chris Getsfred <cgetsfred@gmail.com>
2017-07-10 13:04:49 -0500

    docs: add jest example
    Add an example using Jest

---
+++ examples/jest/routes.test.js
@@ -0,0 +1,20 @@
+// routes.test.js
+const { get } = require('microrouter');
+const { createServer } = require('microrouter-test-server');
+
+const routes = [get('/route1', () => 'route1'), get('/route2', () => 'route2')];
+let server;
+
+beforeAll(async () => {
+  server = await createServer(routes);
+});
+
+test('GET route1 should return expected value', async () => {
+  const result = await server.get('/route1');
+  expect(result).toEqual('route1');
+});
+
+test('GET route2 should return expected value', async () => {
+  const result = await server.get('/route2');
+  expect(result).toEqual('route2');
+});
